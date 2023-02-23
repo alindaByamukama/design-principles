7d913d75682f58d425085d0bf50bdcdaaf0dbee5
alindaByamukama <alindabyamukama@gmail.com>
2023-02-23 11:22:25 +0300

    add strategy directory to branch

---
+++ strategy/strategy.ts
@@ -0,0 +1,21 @@
+/**
+ * Feature:
+    * Implementing the login feature
+        * User can login using one of the following methods:
+            * basic auth (email, password)
+            * UID auth (userid, password)
+            * social auth (facebook, twitter)
+            * JSON WEB TOKEN (jwt token) 
+ */
+
+/**
+ * strategy (abstraction)
+    *   login
+ * algorithms
+    * basic auth (email, password)
+    * UID auth (userid, password)
+    * social auth (facebook, twitter)
+    * JSON WEB TOKEN (jwt token) 
+ * context
+ * client (functions)
+ */
